80cc34bc22d07a8e2880676c865698d7508f2972
hitn8757@gmail.com <hitn8752@gmail.com>
2024-04-16 13:53:29 +0900

    #11 FEAT Add SSD read function when it was empty

--- SSD/SSD.cpp
+++ SSD/SSD.cpp
@@ -1,5 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
 
 class SSD {
 public:
-
+	void read(int lba) {
+		std::ofstream outFile("result.txt");
+		outFile << "0x00000000" << std::endl;
+		outFile.close();
+	}
 };
--- SSDTest/test.cpp
+++ SSDTest/test.cpp
@@ -1,7 +1,32 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "../SSD/SSD.cpp"
+
+using namespace std;
+using namespace testing;
+
 TEST(TestCaseName, TestName) {
 	EXPECT_EQ(1, 1);
 	EXPECT_TRUE(true);
+}
+
+TEST(TestSSD, ReadEmptySpace) {
+	std::ofstream outFile("nand.txt");
+	outFile << "" << std::endl;
+	outFile.close();
+
+	SSD ssd;
+	ssd.read(0);
+
+	std::ifstream inFile("result.txt");
+	string line;
+	getline(inFile, line);
+
+	EXPECT_THAT("0x00000000", Eq(line));
 }
