5552e7c5648d6bf49bb7a60eac56d8e06c7eb5c1
KimJinwoo <kjw7453@gmail.com>
2024-04-16 14:40:01 +0900

    Add Write/Read function

--- AppTest/test.cpp
+++ AppTest/test.cpp
@@ -2,12 +2,33 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 #include "../TestShell/TestShell.cpp"
+using namespace testing;
 
 class MockSSD : public ISSD {
 public:
 	MOCK_METHOD(string, read, (int), (override));
 	MOCK_METHOD(void, write, (int, string), (override));
 };
+
+TEST(TestShellTEST, TestWrite) {
+	MockSSD mock_ssd;
+	TestShell shell{ &mock_ssd };
+
+	EXPECT_CALL(mock_ssd, write(0, "0xDEADCODE"))
+		.Times(1);
+
+	shell.write(0, "0xDEADCODE");
+}
+TEST(TestShellTEST, TestRead) {
+	MockSSD mock_ssd;
+	TestShell shell{ &mock_ssd };
+
+	EXPECT_CALL(mock_ssd, read(0))
+		.Times(1)
+		.WillOnce(Return(string("0x01234567")));
+
+	EXPECT_EQ(shell.read(0), "0x01234567");
+}
 
 TEST(TestShellTEST, TESTFullRead) {
 	MockSSD mock_ssd;
