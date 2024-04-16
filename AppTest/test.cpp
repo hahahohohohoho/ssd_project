48e8375327fece54803b2606e688330d1e2299a9
dudri63 <dudri63@naver.com>
2024-04-16 14:49:03 +0900

    first commit by dudri63

--- AppTest/test.cpp
+++ AppTest/test.cpp
@@ -1,7 +1,11 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+#include "../TestShell/TestShell.cpp"
 
-TEST(TestCaseName, TestName) {
-	EXPECT_EQ(1, 1);
-	EXPECT_TRUE(true);
+using namespace testing;
+using namespace std;
+
+TEST(TestCaseName, exitTest) {
+	
+
 }
---
+++ TestShell/TestShell.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+class TestShell {
+public:
+	void cli(string str) {
+
+	}
+};
