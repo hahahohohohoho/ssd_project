12a1ae0053cdb111a2bf8a0daa50a025f481edb5
seungho <hobin5063@gmail.com>
2024-04-17 10:16:32 +0900

    Add write aging test

--- TestShell/TestShell.cpp
+++ TestShell/TestShell.cpp
@@ -41,6 +41,9 @@ public:
 				cin >> lba >> value;
 				write(lba, value);
 			}
+			else if (command == "waging") {
+				writeAging();
+			}
 			else if (command == "exit") {
 				terminateProcess();
 			}
@@ -93,10 +96,39 @@ public:
 			<< "-- {data} : hexadecimal \n"
 			<< "-- ex. write 3 0xAAAABBBB\n"
 			<< "- read {no} : Read LBA {no} times\n"
-			<< "- fullwrite {value} : 0~99 LBA Write Test\n"
-			<< "- fullread : 0~99 LBA Read Test\n"
+			<< "- fullwrite {value} : 0~99 LBA Write\n"
+			<< "- fullread : 0~99 LBA Read\n"
+			<< "- waging : Write Aging Test\n"
 			<< "- exit : shell exits\n"
 			<< "- help : Displays how to use each command\n";
+	}
+
+	void writeAging() {
+		cout << "[TestApp2] Write Aging Test" << endl;
+
+		cout << "\nStep1 > write lba(0~5) value(0xAAAABBBB)" << endl;
+		for (int lba = 0; lba <= 5; lba++) {
+			for (int i = 0; i < 30; i++) {
+				write(lba, "0xAAAABBBB");
+			}
+		}
+
+		cout << "\nStep2 > overwrite lba(0~5) value(0x12345678)" << endl;
+		for (int lba = 0; lba <= 5; lba++) {
+			write(lba, "0x12345678");
+		}
+
+		cout << "\nStep3 > compare lba(0~5) value(0x12345678)" << endl;
+		for (int lba = 0; lba <= 5; lba++) {
+			string value = ssd->read(lba);
+			cout << "Read Value(lba:" << lba << ") :" << value << endl;
+			if (value != "0x12345678")
+			{
+				cout << "\n[DONE] TEST FAILED!" << endl;
+				return;
+			}
+		}
+		cout << "\n[DONE] TEST SUCCESS!" << endl;
 	}
 
 private:
