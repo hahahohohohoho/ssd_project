1ae089b08731901ded051280932e2a54dd856c36
Sihwan Lee <sihwan14.lee@samsung.com>
2024-04-17 10:10:09 +0900

    #18 REFACTOR Add TestSSDFixture

--- SSDTest/test.cpp
+++ SSDTest/test.cpp
@@ -17,112 +17,111 @@ public:
 	MOCK_METHOD(void, write, (int, string), (override));
 };
 
-class FileManager {
+class TestSSDFixture : public Test {
 private:
 	const string TEST_NAND_FILE_PATH = "nand.txt";
 	const string TEST_RESULT_FILE_PATH = "result.txt";
 
-	ofstream mNandOs;
-	ifstream mNandIs;
-	ifstream mResultIs;
-public:
-	FileManager() {
-		mNandOs.open(TEST_NAND_FILE_PATH);
-		mNandIs.open(TEST_NAND_FILE_PATH);
-		mResultIs.open(TEST_RESULT_FILE_PATH);
-	}
-
-	~FileManager() {
+	void initNand() {
+		ofstream mNandOs(TEST_NAND_FILE_PATH);
+		for (int i = 0; i < 100; i++) {
+			mNandOs << "0x00000000" << "\n";
+		}
 		mNandOs.close();
-		mNandIs.close();
-		mResultIs.close();
+	}
+	void initResult() {
+		ofstream mResultOs(TEST_RESULT_FILE_PATH);
+		mResultOs << "0x00000000";
+		mResultOs.close();
+	}
+public:
+	void SetUp() override {
+		initNand();
+		initResult();
 	}
 
-	void writeNand(string content) {
-		mNandOs << content << "Wn";
+	void TearDown() override {
+		initNand();
+		initResult();
+	}
+
+	void writeNand(int address, string data) {
+		string nandData[100];
+
+		ifstream nandIs("nand.txt");
+		string line;
+		int index = 0;
+		while (getline(nandIs, line) && line != "") {
+			nandData[index++] = line;
+		}
+		nandIs.close();
+
+		nandData[address] = data;
+
+		ofstream nandOs(TEST_NAND_FILE_PATH);
+		for (int i = 0; i < 100; i++) {
+			nandOs << nandData[i] << "\n";
+		}
+		nandOs.close();
 	}
 
 	string readNand(int address) {
+		ifstream nandIs(TEST_NAND_FILE_PATH);
 		int lineNumber = 0;
 		string line;
-		while (getline(mNandIs, line)) {
+		while (getline(nandIs, line)) {
 			if (lineNumber == address) {
+				nandIs.close();
 				return line;
 			}
 			lineNumber++;
 		}
+		nandIs.close();
 		return "";
 	}
 
-	string readResult(int address) {
-		int lineNumber = 0;
+	string readResult() {
+		ifstream resultIs(TEST_RESULT_FILE_PATH);
 		string line;
-		while (getline(mResultIs, line)) {
-			if (lineNumber == address) {
-				return line;
-			}
-			lineNumber++;
-		}
-		return "";
+		getline(resultIs, line);
+		resultIs.close();
+		return line;
 	}
 };
 
-TEST(TestSSDwrite, normal) {
+TEST_F(TestSSDFixture, normal) {
 	SSD ssd;
-	FileManager fileManager;
 
 	ssd.write(3, "0x00000001");
 
-	EXPECT_EQ("0x00000001", fileManager.readNand(3));
+	EXPECT_EQ("0x00000001", readNand(3));
 }
 
-TEST(TestSSD, ReadEmptySpace) {
-	std::ofstream outFile("nand.txt");
-	outFile << "" << std::endl;
-	outFile.close();
-
+TEST_F(TestSSDFixture, ReadEmptySpace) {
 	SSD ssd;
+
 	ssd.read(0);
 
-	std::ifstream inFile("result.txt");
-	string line;
-	getline(inFile, line);
-	inFile.close();
-
-	EXPECT_THAT("0x00000000", Eq(line));
+	EXPECT_THAT(readResult(), "0x00000000");
 }
 
-TEST(TestSSD, ReadZeroLBA) {
-	std::ofstream outFile("nand.txt");
-	outFile << "0x12345678" << std::endl;
-	outFile.close();
-
+TEST_F(TestSSDFixture, ReadZeroLBA) {
 	SSD ssd;
+	writeNand(0, "0x12345678");
+
 	ssd.read(0);
 
-	std::ifstream inFile("result.txt");
-	string line;
-	getline(inFile, line);
-	inFile.close();
-
-	EXPECT_THAT("0x12345678", Eq(line));
+	EXPECT_THAT(readResult(), "0x12345678");
 }
 
-TEST(TestSSD, ReadOneLBA) {
-	std::ofstream outFile("nand.txt");
-	outFile << "0x12345678" << endl;
-	outFile << "0xABCDEFAB" << endl;
-	outFile.close();
-
+TEST_F(TestSSDFixture, ReadOneLBA) {
 	SSD ssd;
+	writeNand(0, "0x12345678");
+	writeNand(1, "0xABCDEFAB");
+
 	ssd.read(1);
 
-	std::ifstream inFile("result.txt");
-	string line;
-	getline(inFile, line);
-	inFile.close();
-
-	EXPECT_THAT("0xABCDEFAB", Eq(line));
+	EXPECT_THAT(readResult(), "0xABCDEFAB");
 }
 
 TEST(TestSSD, AppInvalidArgument) {
