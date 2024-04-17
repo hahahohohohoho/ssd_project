3bbff3adf17f37585b47f10e238dbf3d3fe0e093
Sihwan Lee <sihwan14.lee@samsung.com>
2024-04-17 11:45:04 +0900

    #18 REFACTOR Extract invalid argument checking method

--- SSD/SSD.cpp
+++ SSD/SSD.cpp
@@ -15,16 +15,32 @@ public:
 };
 
 static const int SSD_MAX_DATA_SIZE = 100;
+static const int SSD_FIXED_DATA_LENGTH = 10;
+static const string SSD_DEFAULT_DATA = "0x00000000";
 
 class SSD : public ISSD {
 private:
 	string mData[SSD_MAX_DATA_SIZE];
 	DataArrayFile nandFile{ "nand.txt" };
 	DataArrayFile resultFile{ "result.txt" };
+
+	bool isInvalidData(string data) {
+		if (data.length() != SSD_FIXED_DATA_LENGTH || data.substr(0, 2) != "0x") {
+			return true;
+		}
+		return false;
+	}
+
+	bool isInvalidLba(int lba) {
+		if (lba < 0 || lba > (SSD_MAX_DATA_SIZE - 1)) {
+			return true;
+		}
+		return false;
+	}
 public:
 	SSD() {
 		for (int i = 0; i < SSD_MAX_DATA_SIZE; ++i) {
-			mData[i] = "0x00000000";
+			mData[i] = SSD_DEFAULT_DATA;
 		}
 		if (!nandFile.isCreatedFile()) {
 			nandFile.writeFileLines(mData, SSD_MAX_DATA_SIZE);
@@ -35,16 +51,13 @@ public:
 		nandFile.readFileLines(mData, SSD_MAX_DATA_SIZE);
 		resultFile.writeFileLines(&mData[lba], 1);
 	}
-  
+
 	void write(int lba, string data) {
-    if (data.length() != 10) {
-			throw invalid_argument("Invalid data length");
+		if (isInvalidData(data)) {
+			throw invalid_argument("Invalid data");
 		}
-		if (data.substr(0, 2) != "0x") {
-			throw invalid_argument("Invalid data format");
-		}
-		if (lba < 0 || lba > 99) {
-			throw invalid_argument("Invalid address");
+		if (isInvalidLba(lba)) {
+			throw invalid_argument("Invalid LBA");
 		}
   
 		nandFile.readFileLines(mData, SSD_MAX_DATA_SIZE);
