fd898cfb9144dde9b5797945840a04ff417b62a0
it-hwang <60398229+it-hwang@users.noreply.github.com>
2024-04-18 14:29:20 +0900

    #72 FIX fix flush command

--- SSD/main.cpp
+++ SSD/main.cpp
@@ -4,7 +4,7 @@ void makeCmdList(Application& app) {
 	app.addCommand(new ReadCommand("Read", "R", 3));
 	app.addCommand(new WriteCommand("Write", "W", 4));
 	app.addCommand(new EraseCommand("Erase", "E", 4));
-	app.addCommand(new EraseCommand("Flush", "F", 2));
+	app.addCommand(new FlushCommand("Flush", "F", 2));
 }
 
 int main(int argc, char* argv[]) {
